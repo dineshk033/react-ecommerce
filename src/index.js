150c187975a4bc51f376580ef61d746797cf71dc
dinesh <dineshk033@gmail.com>
2022-11-26 19:15:06 +0530

    Routing added

--- src/index.js
+++ src/index.js
@@ -9,9 +9,14 @@ import "material-icons/iconfont/material-icons.css";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
